9d9dd3c6f6f9c79751f912d46595241f94a31cd5
Thomas <thomas.mirabile@ynov.com>
2024-07-29 18:49:15 +0200

    Add JwtAuthenticationFilter for real

---
+++ src/main/java/com/example/rental/config/JwtAuthenticationFilter.java
@@ -0,0 +1,51 @@
+package com.example.rental.config;
+
+import com.example.rental.service.JwtService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    private final AuthenticationManager authenticationManager;
+    private final JwtService jwtService;
+
+    public JwtAuthenticationFilter(AuthenticationManager authenticationManager, JwtService jwtService) {
+        this.authenticationManager = authenticationManager;
+        this.jwtService = jwtService;
+        setFilterProcessesUrl("/api/auth/login");
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
+            throws AuthenticationException {
+        try {
+            Map<String, String> credentials = new ObjectMapper().readValue(request.getInputStream(), HashMap.class);
+            UsernamePasswordAuthenticationToken authenticationToken =
+                    new UsernamePasswordAuthenticationToken(credentials.get("email"), credentials.get("password"));
+
+            return authenticationManager.authenticate(authenticationToken);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException {
+        UserDetails userDetails = (UserDetails) authResult.getPrincipal();
+        String token = jwtService.generateToken(userDetails);
+
+        response.addHeader("Authorization", "Bearer " + token);
+    }
+}
