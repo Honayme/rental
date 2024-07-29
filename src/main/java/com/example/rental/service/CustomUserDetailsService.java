b15d9746bf5b01344c874d7964a8ffb73b4160d1
Thomas <thomas.mirabile@ynov.com>
2024-07-29 18:36:36 +0200

    add CustomUserDetailsService

---
+++ src/main/java/com/example/rental/service/CustomUserDetailsService.java
@@ -0,0 +1,36 @@
+package com.example.rental.service;
+
+import com.example.rental.entities.UserEntity;
+import com.example.rental.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        UserEntity userEntity = userRepository.findByEmail(email);
+        if (userEntity == null) {
+            throw new UsernameNotFoundException("User not found with email: " + email);
+        }
+
+        return User.withUsername(userEntity.getEmail())
+                .password(userEntity.getPassword())
+                .authorities(new ArrayList<>()) // Ajoutez des autorités si nécessaire
+                .build();
+    }
+}
